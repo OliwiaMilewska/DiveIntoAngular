5491933131dc1bebabb5dba846147e263f4c6878
Oliwia <liwiamilka@gmail.com>
2023-06-23 10:55:48 +0200

    Using map operator with observables.

--- dive-into-angular/src/app/observables/home-obs/home-obs.component.ts
+++ dive-into-angular/src/app/observables/home-obs/home-obs.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-obs',
   templateUrl: './home-obs.component.html',
   styleUrls: ['./home-obs.component.css']
 })
-export class HomeObsComponent {
+export class HomeObsComponent implements OnInit, OnDestroy {
+  sub!: Subscription;
+  customSub!: Subscription;
 
+  ngOnInit(): void {
+    this.sub = interval(1000).subscribe((count: number) => console.log(count));
+
+    // Custom Observable
+    const customIntervalObservable = new Observable((observer: { next: (count: number) => void; error: (error: Error) => void; complete: () => void; }) => {
+      let count: number = 0
+      setInterval(() => {
+        observer.next(count);
+        if (count == 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.customSub = customIntervalObservable.pipe(map((data: number) => {
+      return 'Round: ' + (data + 1);
+    }))
+      .subscribe(
+        (data: string) => console.log(data),
+        (error: string) => alert(error),
+        () => console.log('Completed custom observable.'));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.customSub.unsubscribe();
+  }
 }
