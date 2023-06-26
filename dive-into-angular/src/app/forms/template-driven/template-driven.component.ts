8d340a56b0b1732e0652d5009b7934885c58f1bb
Oliwia <liwiamilka@gmail.com>
2023-06-26 10:15:42 +0200

    Validating template.

--- dive-into-angular/src/app/forms/template-driven/template-driven.component.ts
+++ dive-into-angular/src/app/forms/template-driven/template-driven.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./template-driven.component.css']
 })
 export class TemplateDrivenComponent {
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  // Option 1
+  // onSubmit(form: NgForm): void {
+  //   console.log("Local reference");
+  //   console.log(form);
+  // }
 
+  // Option 2: Used when you need to access the form before you submit it. 
+  @ViewChild('f') form!: NgForm;
+
+  onSubmit(): void {
+    console.log("View child");
+    console.log(this.form);
+  }
+
+  suggestUsername() {
+
+  }
 }
