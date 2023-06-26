b9c0d59bb42b25ea878ef2a222525307e84b49ff
Oliwia <liwiamilka@gmail.com>
2023-06-26 10:37:31 +0200

    Reading data from form.

--- dive-into-angular/src/app/forms/template-driven/template-driven.component.ts
+++ dive-into-angular/src/app/forms/template-driven/template-driven.component.ts
@@ -9,6 +9,17 @@ import { NgForm } from '@angular/forms';
 export class TemplateDrivenComponent {
   defaultQuestion: string = 'pet';
   answer: string = '';
+  genders: string[] = ['female', 'male'];
+  suggestedName: string = '';
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted: boolean = false;
+
   // Option 1
   // onSubmit(form: NgForm): void {
   //   console.log("Local reference");
@@ -16,14 +27,43 @@ export class TemplateDrivenComponent {
   // }
 
   // Option 2: Used when you need to access the form before you submit it. 
-  @ViewChild('f') form!: NgForm;
+  @ViewChild('f') signUpForm!: NgForm;
 
   onSubmit(): void {
     console.log("View child");
-    console.log(this.form);
+    console.log(this.signUpForm);
+
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.submitted = true;
+
+    this.onResetForm();
   }
 
-  suggestUsername() {
+  setDefaultValues(): void {
+    this.signUpForm.setValue({
+      userData: {
+        username: 'Teścikowy',
+        email: 'test@email.com'
+      },
+      secret: 'pet',
+      questionAnswer: 'This is mine answer.',
+      gender: 'female'
+    })
+  }
 
+  suggestUsername(): void {
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: 'Rex'
+      }
+    });
+  }
+
+  onResetForm(): void {
+    this.signUpForm.reset();
   }
 }
