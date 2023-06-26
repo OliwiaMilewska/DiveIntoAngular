e2b929ef04b4378e6e1a1c1d0395274676472c75
Oliwia <liwiamilka@gmail.com>
2023-06-26 14:56:07 +0200

    Async validation.

--- dive-into-angular/src/app/forms/reactive/reactive.component.ts
+++ dive-into-angular/src/app/forms/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive',
@@ -23,7 +24,8 @@ export class ReactiveComponent implements OnInit {
     this.signUpForm = new FormGroup({
       userData: new FormGroup({
         username: new FormControl(null, [Validators.required, Validators.minLength(3), this.forbiddenNames.bind(this)]),
-        email: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        // email: new FormControl(null, [Validators.required, Validators.email] , this.forbiddenEmails), using async validator
       }),
       gender: new FormControl('female'),
       hobbies: new FormArray([])
@@ -56,5 +58,18 @@ export class ReactiveComponent implements OnInit {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1)
       return { 'nameIsForbidden': true };
     return { 'nameIsForbidden': false };
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailIsforbbiden': true });
+        }
+        else {
+          resolve(null)
+        }
+      }, 1500);
+    });
   }
 }
