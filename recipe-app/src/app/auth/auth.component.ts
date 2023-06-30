49066e95aceaf01d52493c2639b2eb7659464693
Oliwia <liwiamilka@gmail.com>
2023-06-30 17:45:50 +0200

    Course Project - Sign up and login in to firebase endpoints and functionality with  template driven form.

--- recipe-app/src/app/auth/auth.component.ts
+++ recipe-app/src/app/auth/auth.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthResponseData, AuthService } from '../shared/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent{
+  isLoginMode: boolean = true;
+  isLoading: boolean = false;
+  error: string = '';
 
+  constructor(private _authService: AuthService) { }
+
+  onSwitchMode(): void {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm): void {
+    if (!form.valid) return;
+    let email: string = form.value.email;
+    let password: string = form.value.password;
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      authObs = this._authService.logIn(email, password);
+    }
+    else {
+      authObs = this._authService.signUp(email, password);
+    }
+
+    authObs.subscribe(() => this.isLoading = false,
+      errorResponse => {
+        this.error = errorResponse;
+        this.isLoading = false;
+        setTimeout(() => {
+          this.error = '';
+        }, 3000);
+      });
+
+    form.reset();
+  }
 }
---
+++ recipe-app/src/app/shared/services/auth.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+export interface AuthResponseData {
+  kind: string,
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string,
+  registered?: boolean
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private _http: HttpClient) { }
+
+  signUp(email: string, password: string): Observable<AuthResponseData> {
+    return this._http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBXzMP9Ve3jWo68v3eR2npuytZWBz7qnNs', {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).pipe(catchError(this.handleError));
+  }
+
+  logIn(email: string, password: string): Observable<AuthResponseData> {
+    return this._http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBXzMP9Ve3jWo68v3eR2npuytZWBz7qnNs', {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError('An error occured.')
+    }
+
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS': //Messages from docs of firebase
+        errorMessage = 'This email exsists already';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is invalid for this email';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'Account was disabled by administrator.';
+        break;
+        case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exsists.';
+        break;
+      default:
+        errorMessage = 'An unknown error occured.';
+        break;
+    }
+    return throwError(errorMessage)
+  }
+}
